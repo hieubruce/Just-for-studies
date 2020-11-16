f35fa7d32a6e745012b85bf91402ecfcfcf645fa
FIXED-TERM Nguyen Dinh Minh Hieu (RBVH/EDA15) <ing81hc@bosch.com>
2020-11-16 15:49:37 +0700

    Add seeddata

--- eShopSolution/eShopSolution.Data/Entities/Category.cs
+++ eShopSolution/eShopSolution.Data/Entities/Category.cs
@@ -9,7 +9,7 @@ namespace eShopSolution.Data.Entities
     {
         public int Id { get; set; }
         public int SortOrder { get; set; }
-        public bool IsShowOnHone { get; set; }
+        public bool IsShowOnHome { get; set; }
         public int? ParentId { get; set; }
         public Status Status { get; set; }
         public List<CategoryTranslation> CategoryTranslations { get; set; }
